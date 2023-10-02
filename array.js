2698171309b29651f7965be0e9fa7934ba9ffae7
Hayden <nokdujeon@outlook.com>
2023-10-02 18:36:51 +0700

    refactor: refactor array.js add the following new functions: 1. createArray() 2. accessArrayElement() 3. changeArrayElement() 4. builtinArrayPropertiesAndMethods()

--- array.js
+++ array.js
@@ -1,62 +1,57 @@
 const { arrays } = require('./global_variables');
-// An array is a special variable, which can hold more than one value
 
-// Creating an Array
-// Using an array literal is the easiest way to create a JavaScript Array.
-// Example
-const values = ['Saab', 'Volvo', 'BMW'];
-// It is a common practice to declare arrays with the const keyword.
+// An array is a special variable, which can hold more than one value.
+// Why Use Arrays? An array can hold many values under a single name, and you can access the values by referring to an index number.
 
-// A declaration can span multiple lines:
-// Example
-const carMakers = [
-    'Saab',
-    'Volvo',
-    'BMW',
-    'Benz',
-    'Audi',
-    'Volkswagen',
-    'Kia',
-    'Hyundai',
-    'Toyota',
-    'Honda',
-];
+function createArray() {
+    // Using an array literal is the easiest way to create a JavaScript Array.
+    const values = ['Saab', 'Volvo', 'BMW'];
+    // It is a common practice to declare arrays with the const keyword.
 
-// You can also create an array, and then provide the elements:
-// Example
-const autoMakers = [];
-autoMakers[0] = 'Saab';
-autoMakers[1] = 'Volvo';
-autoMakers[2] = 'BMW';
+    // A declaration can span multiple lines:
+    const carMakers = [
+        'Saab',
+        'Volvo',
+        'BMW',
+        'Benz',
+        'Audi',
+        'Volkswagen',
+        'Kia',
+        'Hyundai',
+        'Toyota',
+        'Honda',
+    ];
 
-// Using the JavaScript Keyword new
-// The following example also creates an Array, and assigns values to it:
-// Example
-const vehicleMakers = new Array('Saab', 'Volvo', 'BMW');
+    // You can also create an array, and then provide the elements:
+    const autoMakers = [];
+    autoMakers[0] = 'Saab';
+    autoMakers[1] = 'Volvo';
+    autoMakers[2] = 'BMW';
 
-// The two examples above do exactly the same.
-// There is no need to use new Array().
-// For simplicity, readability and execution speed, use the array literal method.
+    // Using the JavaScript Keyword new
+    // The following example also creates an Array, and assigns values to it:
+    const vehicleMakers = new Array('Saab', 'Volvo', 'BMW');
 
-// Why Use Arrays?
-// An array can hold many values under a single name, and you can access the values by referring to an index number.
+    // The two examples above do exactly the same.
+    // There is no need to use new Array().
+    // For simplicity, readability and execution speed, use the array literal method.
+}
 
-// Accessing Array Elements
-// You access an array element by referring to the index number:
-// Example
-// Accessing the First Array Element
-carMakers[0];
-// Accessing the Last Array Element
-carMakers[carMakers.length - 1];
+function accessArrayElement() {
+    // You access an array element by referring to the index number:
+    // Accessing the First Array Element
+    arrays.animals[0];
+    // Accessing the Last Array Element
+    arrays.animals[arrays.animals.length - 1];
+}
 
-// Changing an Array Element
-// This statement changes the value of the first element in cars:
-// Example
-carMakers[0] = 'Opel';
+function changeArrayElement() {
+    // This statement changes the value of the first element in animals:
+    arrays.animals[1] = 'Pig';
+}
 
-// The built-in Array Properties and Methods
-// The real strength of JavaScript arrays are the built-in array properties and methods:
-// Example
-carMakers.length; // The `length` property of an array returns the length of an array (the number of array elements).
-carMakers.sort(); // Sorts the array
-console.count(arrays.animals.sort());
+function builtinArrayPropertiesAndMethods() {
+    // The real strength of JavaScript arrays are the built-in array properties and methods:
+    arrays.animals.length; // The `length` property of an array returns the length of an array (the number of array elements).
+    arrays.animals.sort(); // Sorts the array
+}
