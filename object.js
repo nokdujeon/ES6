c1f2df605af0ccecc3e6d7f8324a929bddec1520
Hayden <nokdujeon@outlook.com>
2023-10-03 12:42:27 +0700

    refactor: refactor object.js 1. useLiterals() 2. explainProperties()

--- object.js
+++ object.js
@@ -11,11 +11,11 @@
 // All cars have the same properties, but the property values differ from car to car.
 // All cars have the same methods, but the methods are performed at different times.
 
-function object() {
+function explainObject() {
     // An object is a collection of unordered properties, and a property is an association between a name(also called a key) and a value.
     // A JavaScript object is a data structure that stores a collection of properties.
 
-    const batman = {
+    const objectVariable = {
         // Objects are variables too. But objects can contain many values.
         // Tshis code assigns many values ('Bruce Wayne', 33) to a variable named variable:
         name: 'Bruce Wayne',
@@ -25,44 +25,44 @@ function object() {
 
     function create() {
         function useLiterals() {
-            const object = {
-                key: value, //  key_1: value_1, ... , key_n: value_n
+            const objectUsingLiterals = {
+                // name: value, //  name_1: value_1, ... , name_n: value_n
                 // The name: values pairs in JavaScript objects are called properties:
                 // property, ... , property,
             };
         }
     }
 
-    function properties() {
+    function explainProperties() {
         // A JavaScript property is a member of an object that associates a key with a value.
         // Type of key of property should be string or Symbol.
         // A property's value can be a function, in which case the property is known as a method.
 
         // A property consists of the following parts:
-        function name() {
-            // 1) A name (also called a key), which is either a string or a symbol.
-        }
-        function value() {
-            // 2) A value, which can be any JavaScript value. A property that has a function as its value may also be called a method.
-        }
-        // 3) Some attributes, which specify how the property can be read and written. A property may have the configurable, enumerable, and writable attributes.
+        // 1) A name (also called a key), which is either a string or a symbol.
+        const names = [];
+
+        // 2) A value, which can be any JavaScript value. A property that has a function as its value may also be called a method.
+        const values = [];
+
+        // 3) Some attributes, which specify how the property can be read and written. A property may have the `configurable`, `enumerable`, and `writable` attributes.
 
         function accessProperties() {
-            // Accessing Object Properties
-            batman.name; // 'Bruce Wayne'
-            batman['age']; // 33
+            // Accessing Properties
+            objectVariable.name; // 'Bruce Wayne'
+            objectVariable['age']; // 33
         }
 
         function addNewProperties() {
             // Adding New Properties
-            batman.gender = 'male';
-            batman['hairColor'] = 'black';
+            objectVariable.gender = 'male';
+            objectVariable['hairColor'] = 'black';
         }
 
         function deleteProperties() {
             // Deleting Properties
-            delete batman.hairColor;
-            delete batman['gender'];
+            delete objectVariable.hairColor;
+            delete objectVariable['gender'];
             // The delete operator should not be used on predefined JavaScript object properties. It can crash your application.
         }
 
